4ddabf80fd0cea40337bb2622ff6bee75c352e09
unknown <xfedork1@ad.fi.muni.cz>
2024-04-18 18:42:38 +0200

    fix: few of codacy scanning issues

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 import os
 import yaml
 import flask
@@ -24,11 +24,18 @@ def print_nametag(format_string, person):
 
 
 def fetch_website(urllib_version, url):
+    """Fetches website.
+
+    Args:
+        urllib_version (str): url library version
+        url (str): url of website
+
+    """
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
-    try: 
+    try:
         http = urllib.PoolManager()
         r = http.request('GET', url)
     except:
@@ -52,8 +59,8 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
-        new_person = Person("Vickie")  
+    if choice == "1":
+        new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
